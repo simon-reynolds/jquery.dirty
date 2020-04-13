bdd929a946bfad8bcc9b554ace7235b532e1345c
Simon Taite <simontaite1@gmail.com>
2020-04-13 13:38:29 +0100

    TEMP - move to typescript

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+
+gulp.task('default', function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/jquery.d.ts', 'src/jquery.dirty.d.ts', 'src/jquery.dirty.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(source('jquery.dirty.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+});
